1da280f0e5c37f4b01d8e9a9c28e322ae07f6b73
Mslibi <moslibi@hotmail.co.uk>
2026-08-11 20:15:24 +0100

    Exercise 6: implement ScreenResultPrinter to output one line per trade

--- cpp/RiskSystem/ScreenResultPrinter.cpp
+++ cpp/RiskSystem/ScreenResultPrinter.cpp
@@ -3,6 +3,25 @@
 
 void ScreenResultPrinter::printResults(ScalarResults& results) {
     for (const auto& result : results) {
+
+        std::cout << result.getTradeId();
+
+        // If there is a result
+        if (result.getResult().has_value()) {
+            std::cout << " : " << result.getResult().value();
+        }
+        // If there is an Error
+        if (result.getError().has_value()) {
+            if (!result.getError().value().empty()) {
+                std::cout << " : " << result.getError().value() << std::endl;
+            }
+            else {
+                std::cout << std::endl;
+            }
+        }
+        else {
+            std::cout << std::endl;
+        }
         // Write code here to print out the results such that we have:
         // TradeID : Result : Error
         // If there is no result then the output should be:
